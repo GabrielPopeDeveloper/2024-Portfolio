339715ace75a11f5626c374b7b6279910250aac1
Gabriel Pope <gabrielpopedeveloper@gmail.com>
2024-04-04 23:44:49 -0700

    Added multiple files and beginner styles.

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll(".navigation a");
+    
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust offset as needed
+                behavior: "smooth"
+            });
+        });
+    });
+});
